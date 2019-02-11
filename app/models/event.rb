c17daa85822905a35946268738c0ab1526def7b6
jiaad <abdulziad08@outlook.com>
2019-02-12 00:16:39 +0100

    ajout de validates

--- app/models/event.rb
+++ app/models/event.rb
@@ -9,8 +9,8 @@ class Event < ApplicationRecord
 
     validates :start_date, presence:true
     validates :duration , presence:true
-    validates :title, presence:true
-    validates :description , presence:true
-    validates :price , presence:true
+    validates :title, presence:true,length: {in: 3...50}
+    validates :description , presence:true, length:{minimum: 5}
+    validates :price , presence:true,:format => { :with => /^(\$)?(\d+)(\.|,)?\d{0,2}?$/ }
     validates :location, presence:true
 end
