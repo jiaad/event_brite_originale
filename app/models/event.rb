afeaf86c545c53b516582838c797c7f79ab9cee7
jiaad <abdulziad08@outlook.com>
2019-02-12 10:03:59 +0100

    r

--- app/models/event.rb
+++ app/models/event.rb
@@ -11,6 +11,6 @@ class Event < ApplicationRecord
     validates :duration , presence:true
     validates :title, presence:true,length: {in: 3...50}
     validates :description , presence:true, length:{minimum: 5}
-    validates :price , presence:true,:format => { :with => /^(\$)?(\d+)(\.|,)?\d{0,2}?$/ }
+    validates :price , presence:true
     validates :location, presence:true
 end
