a561afaad1f8fccfe5582815131f3ca19c2d127b
jiaad <abdulziad08@outlook.com>
2019-02-13 14:35:06 +0100

    rien du tout

--- db/migrate/20190211105802_create_users.rb
+++ db/migrate/20190211105802_create_users.rb
@@ -3,9 +3,9 @@ class CreateUsers < ActiveRecord::Migration[5.2]
     create_table :users do |t|
       t.string :first_name
       t.string :last_name
-      t.string :email
+      # t.string :email
       t.text :description
-      t.string :encrypted_password
+      # t.string :encrypted_password
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -40,11 +40,11 @@ ActiveRecord::Schema.define(version: 2019_02_12_152332) do
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
-    t.string "email"
     t.text "description"
-    t.string "encrypted_password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
